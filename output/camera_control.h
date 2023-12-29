4efd8e0e97fdef783639dca5af24ce26eb9d0c78
obauern <obauern@gmail.com>
2023-12-29 17:41:03 +0100

    continuous focus added. defines with names

--- output/camera_control.h
+++ output/camera_control.h
@@ -5,9 +5,10 @@
 #include <stdbool.h>
 #include "tm4c_cmsis.h"
 #include "TM4C123GH6PM.h"
+#include "sensor_control.h"
 
 
-void camera_control(bool isInputTriggered);
+void camera_control(sensorParameters_t* sensorParametersPtr);
 bool isPictureTaked(void);
 
 #endif /*__CAMERA_CONTROL_H__*/
