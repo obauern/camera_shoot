51dfc8c849a05454962cc88e60760ea390f2e14a
obauern <obauern@gmail.com>
2024-03-29 13:32:25 +0100

    port b added

--- HAL/GPIO/GpioPortB.c
+++ HAL/GPIO/GpioPortB.c
@@ -3,6 +3,9 @@
 #include "TM4C123GH6PM.h"
 #include "tm4c_cmsis.h"
 
+#define PB0_OUTPUT (1U << 0U)
+#define PB1_INPUT  (~(1U << 1U))
+
 #define PB2_INPUT (~(1U << 2U))
 #define PB3_OUTPUT (1U << 3U)
 
@@ -15,14 +18,19 @@
 
 #define PB2_GPIO_PIN  (1U << 2U)
 
+#define CLOCK_PORTB_AVAILABLE (1U << 1U)
+
 static void pb2T3CCP0Pb3Trigger(void);
+static void initChipSelectAndTouchIrq(void);
 
 void GpioPortB_init(void)
 {
     SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1;   /*gpio clock gate for port B*/
+    while (!(SYSCTL_RCGCGPIO_R & CLOCK_PORTB_AVAILABLE));
     SYSCTL_GPIOHBCTL_R |= SYSCTL_GPIOHBCTL_PORTB; /*activates the ahb*/
     
     pb2T3CCP0Pb3Trigger();
+    initChipSelectAndTouchIrq();
 }
 
 void HalPortB_activatePin(uint8_t pin)
@@ -38,10 +46,6 @@ void HalPortB_deactivatePin(uint8_t pin)
 bool GpioPortB_readPin(uint8_t pin)
 {
     bool retVal = false;
-    if(pin != PB2_GPIO_PIN)
-    {
-        return retVal;
-    }
     
     if ((GPIOB_HS->DATA_Bits[pin] & pin) == 0U)
     {
@@ -67,6 +71,21 @@ static void pb2T3CCP0Pb3Trigger(void)
     
     GPIOB_HS->PCTL &= ~0x00000F00;
     GPIOB_HS->PCTL |= PB2_T3CCP0;
+}
+
+static void initChipSelectAndTouchIrq(void)
+{
+      SYSCTL_GPIOHBCTL_R |= SYSCTL_GPIOHBCTL_PORTB;
+    // Configura pines para SSI0 (Puedes ajustar según tu necesidad)
+    GPIOB_HS->DIR = 0x00;
+    GPIOB_HS->DIR |= PB0_OUTPUT;
+    GPIOB_HS->DIR &= PB1_INPUT;  // PB0(output): Chip Select (Touch) PB1(input): Touch Irq
+
+    GPIOB_HS->DEN = 0x00;
+    GPIOB_HS->DEN |= (TOUCH_CS_Pin | TOUCH_IRQ_Pin); /* PB0 y PB1 digital pins*/
+    
+    GPIOB_HS->PUR = 0x00;
+    GPIOB_HS->PUR |= (TOUCH_CS_Pin | TOUCH_IRQ_Pin); /* PULL-UP both pins */
 }
 
 
