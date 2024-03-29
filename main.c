4aad6e4115d362c3aa91f8131a2c22588ae3373d
obauern <obauern@gmail.com>
2024-03-29 16:12:02 +0100

    init touch added

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "tm4c_cmsis.h"
-#include "Service/Init/init.h"
+
 #include "Application/Camera/output/CameraControl.h"
 #include "Application/Camera/input/SensorControl.h"
+#include "Service/TouchPanel/Panel/ili9341.h"
+#include "Service/TouchPanel/Touch/ili9341Touch.h"
+#include "Service/Init/init.h"
 
 int main()
 {
     initialise_registers();
+    ILI9341Touch_init();
+    ILI9341_Init();
     CameraControl_init();
     
     while(true)
